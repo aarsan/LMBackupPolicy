84e7c8d9e0441639f378892e3fd2972e5a2e6d4f
Ahmet Arsan <aarsan@progger.com>
2025-05-23 05:47:20 -0500

    initial commit

---
+++ backup_ci.ps1
@@ -0,0 +1,10 @@
+write-host "Importing Az module"
+# Import-Module Az
+
+
+# Connect to your Azure account
+write-host "Connecting to Azure account"
+Connect-AzAccount
+
+
+New-AzResourceGroupDeployment -ResourceGroupName "LibertyMutualDemo" -TemplateFile "./template.json" -TemplateParameterFile "./parameters.json"
